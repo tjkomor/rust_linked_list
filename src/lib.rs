7c3a8f1c7b56ccbd6d0122972f17be916b2e519c
tyler komoroske <tjkomor@clemson.edu>
2021-12-04 11:04:32 -0700

    add push fn functionality

--- src/lib.rs
+++ src/lib.rs
@@ -6,7 +6,18 @@ impl LinkedList {
     fn empty() -> LinkedList {
         LinkedList {head: None}
     }
+
+    fn push(&mut self, element: i32) {
+        let old_head: Option<Box<Node>> = self.head.take();
+        let new_head: Box<Node> = Box::new(Node { 
+            element, 
+            next: old_head, 
+        });
+
+        self.head = Some(new_head);
+    }
 }
+
 
 struct Node {
     element: i32,
@@ -22,6 +33,7 @@ mod tests {
     use super::*;
     #[test]
     fn it_works() {
-        let list: LinkedList = LinkedList::empty();
+        let mut list: LinkedList = LinkedList::empty();
+        list.push(1);
     }
 }
